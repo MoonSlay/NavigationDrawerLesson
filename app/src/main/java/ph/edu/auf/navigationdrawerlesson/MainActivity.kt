5e86ed148effaa31445c1224e652a1d355ff61f9
Angelo <delrosarioangelo18@gmail.com>
2024-09-26 13:46:12 +0800

    change the way the navigation was handled and implemented the use of mobile_navigation.xml

--- app/src/main/java/ph/edu/auf/navigationdrawerlesson/MainActivity.kt
+++ app/src/main/java/ph/edu/auf/navigationdrawerlesson/MainActivity.kt
@@ -4,19 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import ph.edu.auf.navigationdrawerlesson.databinding.ActivityMainBinding
-import ph.edu.auf.navigationdrawerlesson.fragments.FaveQuotesFragment
-import ph.edu.auf.navigationdrawerlesson.fragments.FunnyQuotesFragment
-import ph.edu.auf.navigationdrawerlesson.fragments.HistoricalQuotesFragment
-import ph.edu.auf.navigationdrawerlesson.fragments.InspirationalQuotesFragment
-import ph.edu.auf.navigationdrawerlesson.fragments.LoveQuotesFragment
-import ph.edu.auf.navigationdrawerlesson.fragments.MotivationalQuotesFragment
-import ph.edu.auf.navigationdrawerlesson.fragments.QuotesOfTheDayFragment
-import ph.edu.auf.navigationdrawerlesson.fragments.TravelQuotesFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,17 +22,21 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(android.R.drawable.ic_menu_more)
 
+        // Setup NavController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.navController
+
         // Handle Navigation Drawer Item Clicks
         binding.navView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.nav_fave_quotes -> openFragment(FaveQuotesFragment())
-                R.id.nav_love_quotes -> openFragment(LoveQuotesFragment())
-                R.id.nav_quotes_day -> openFragment(QuotesOfTheDayFragment())
-                R.id.nav_funny_quotes -> openFragment(FunnyQuotesFragment())
-                R.id.nav_inspirational_quotes -> openFragment(InspirationalQuotesFragment())
-                R.id.nav_motivational_quotes -> openFragment(MotivationalQuotesFragment())
-                R.id.nav_historical_quotes -> openFragment(HistoricalQuotesFragment())
-                R.id.nav_travel_quotes -> openFragment(TravelQuotesFragment())
+                R.id.nav_fave_quotes -> navController.navigate(R.id.nav_fave_quotes)
+                R.id.nav_love_quotes -> navController.navigate(R.id.nav_love_quotes)
+                R.id.nav_quotes_day -> navController.navigate(R.id.nav_quotes_day)
+                R.id.nav_funny_quotes -> navController.navigate(R.id.nav_funny_quotes)
+                R.id.nav_inspirational_quotes -> navController.navigate(R.id.nav_inspirational_quotes)
+                R.id.nav_motivational_quotes -> navController.navigate(R.id.nav_motivational_quotes)
+                R.id.nav_historical_quotes -> navController.navigate(R.id.nav_historical_quotes)
+                R.id.nav_travel_quotes -> navController.navigate(R.id.nav_travel_quotes)
             }
             binding.drawerLayout.closeDrawer(GravityCompat.START)
             true
@@ -46,13 +44,7 @@ class MainActivity : AppCompatActivity() {
 
         // Load default fragment
         if (savedInstanceState == null) {
-            openFragment(FaveQuotesFragment())
-        }
-    }
-
-    private fun openFragment(fragment: Fragment) {
-        supportFragmentManager.commit {
-            replace(R.id.fragment_container_view, fragment) // Ensure you have a correct FragmentContainerView or FrameLayout
+            navController.navigate(R.id.nav_quotes_day)
         }
     }
 
@@ -61,4 +53,5 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 }
+
 
